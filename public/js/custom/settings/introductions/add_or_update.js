0e661d02958b519ae20a84ce97e2a4d256588287
Ammar Esber <ammar.esber29@outlook.com>
2021-07-14 15:05:55 +0300

    delete unwanted codes

--- public/js/custom/settings/introductions/add_or_update.js
+++
@@ -1,5 +0,0 @@
-$("#introduction-upload").on("change", (function (e) {
-    n = $("#introduction-upload-img")
-   var r = new FileReader, a = e.target.files;
-   r.onload = function () { n && n.attr("src", r.result) }, r.readAsDataURL(a[0])
-}));
